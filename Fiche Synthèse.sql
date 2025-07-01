d9ee8f3873e64dab2139f69c4ec11f96c68a1847
Slaxei <alexisquentin@outlook.fr>
2025-07-02 00:32:22 +0200

    Add files via upload

---
+++ Fiche Synthèse.sql
@@ -0,0 +1,14 @@
+Fiche synthèse :
+
+ 3.a. Chiffres d'affaires total = 44825
+
+3.b. Les ventes par produit
+
+Ventes Produit A = 1750
+Ventes Produit B = 1055
+Ventes Produit C = 575
+
+3.c. Les ventes par région
+
+Ventes région du Nord = 1605
+Ventes région du Sud = 1775	
